f70152c95f80b3f674c0a1e1c426bfe156711ef4
Somanathpatil <patilsomanath292@gmail.com>
2025-08-06 20:25:31 +0530

    env

--- app/api/Login/route.js
+++ app/api/Login/route.js
@@ -5,34 +5,34 @@ import bcrypt from 'bcryptjs'
 import { genrateToken } from "@/app/libs/jwtToken";
 
 export async function POST(request) {
-    try{
+    try {
         await connection();
-        const {email, password} = await request.json();
-        const exist = await agent.findOne({email});
-        if(!exist){
-            return NextResponse.json({message:"User Not Found!", status:404});
+        const { email, password } = await request.json();
+        const exist = await agent.findOne({ email });
+        if (!exist) {
+            return NextResponse.json({ message: "User Not Found!", status: 404 });
         }
 
         const currect = await bcrypt.compare(password, exist.password);
-        if(!currect){
-            return NextResponse.json({message:'Password is missmatch', status:401});
+        if (!currect) {
+            return NextResponse.json({ message: 'Password is missmatch', status: 401 });
         }
 
-        const token = genrateToken({userId: exist._id, email: exist.email});
-        const responce =  NextResponse.json({message:'Login Successfull', token}, {status:201})
+        const token = genrateToken({ userId: exist._id, email: exist.email });
+        const responce = NextResponse.json({ message: 'Login Successfull', token }, { status: 201 })
 
         responce.cookies.set('token', token, {
-            httpOnly : true,
-            secure : process.env.NODE_ENV = "Production",
-            maxAge : 60 *60,
+            httpOnly: true,
+            secure: (process.env.NODE_ENV || '').trim() === "production",
+            maxAge: 60 * 60,
             path: '/'
         })
 
 
         return responce;
 
-    }catch(err){
-        return NextResponse.json({message:'API Problem', status:404});
+    } catch (err) {
+        return NextResponse.json({ message: 'API Problem', status: 404 });
     }
 
 }
